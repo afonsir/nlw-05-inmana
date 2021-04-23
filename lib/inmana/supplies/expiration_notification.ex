96c7783dc5045e7f78994a0463bf1e74db261e62
AfonSir <afonsir17@gmail.com>
2021-04-23 15:53:00 -0300

    feat: add project

---
+++ lib/inmana/supplies/expiration_notification.ex
@@ -0,0 +1,16 @@
+defmodule Inmana.Supplies.ExpirationNotification do
+  alias Inmana.Mailer
+  alias Inmana.Supplies.{ExpirationEmail, GetByExpiration}
+
+  def send do
+    GetByExpiration.call()
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    |> Mailer.deliver_later!()
+  end
+end
